4ded40c58e6baa3ea4c8b5411133e02f70b8566a
hafijure <hafijure@gmail.com>
2022-10-26 23:58:28 -0300

    why function used for reusing code and avoiding duplicate code

--- practice.js
+++ practice.js
@@ -1,3 +1,31 @@
+// ************ coding with normal way ******* //
+
+// let nums = [4,5,10,15,13,14,20];
+
+// for (let i = 0; i < nums.length; i++) {
+//     const num = nums[i];
+//     if (num % 2 == 0) {
+//         console.log(num + ' is an even number');
+//     }
+//     else{
+//         console.log(num + ' is an odd number');
+//     }
+    
+// }
+
+// let ages = [15,20,21,24,30,35];
+// for (let i = 0; i < ages.length; i++) {
+//     const age = ages[i];
+//     if (age % 2 == 0) {
+//         console.log(age + ' is an even number');
+//     }
+//     else{
+//         console.log(age + ' is an odd number');
+//     }
+    
+// }
+
+// ************ Function using for avoiding duplicate coding and reusing ******* //
 
 function evenifyAll(nums){
     for (let i = 0; i < nums.length; i++) {
